a54804bd2b0a9c7cea785c0cd32a8d94c8241cbe
awildcatfox <130520774+awildcatfox@users.noreply.github.com>
2024-01-09 13:52:43 -0800

    completed guided assignment

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
 ﻿namespace GA_Class_DavidAbarca
 {
     internal class Program
-    {
+    {  // David Abarca
+       // Guided Assignment: Class
+       // 01/09/2024
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Student student = new Student();
+            Student david = new Student();
+
+            Professor professor = new Professor();
+            Professor will = new Professor();
+
+            Game game = new Game();
+            Game GTA6 = new Game();
+
         }
     }
 }
